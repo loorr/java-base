88c5c3da50114228fc294a980bd86cb8b70918e7
loorr <zjianfa@qq.com>
2022-07-21 20:31:13 +0800

    add: day homework

---
+++ java-base-day4/src/main/java/org/example/day4/SwitchStudy.java
@@ -0,0 +1,21 @@
+package org.example.day4;
+
+public class SwitchStudy {
+    public static void main(String[] args) {
+        int a = 1;
+        switch (a) {
+            case 1:
+                System.out.println("1");
+                break;
+            case 2:
+                System.out.println("2");
+                break;
+            case 3:
+                System.out.println("3");
+                break;
+            default:
+                System.out.println("default");
+                break;
+        }
+    }
+}
